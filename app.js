e1db873ee8ead7db9a4a902046417d8c6af4c7ca
Nickpandiscia <npandiscia1@gmail.com>
2022-11-25 15:41:42 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Nick";
+let lastName = "Yaya";
+var age = 26;
 
-// Exercise 2
+//Exercise 2
+let fullName = firstName + "" + lastName;
+console.log(fullName);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let myStory = `Hello, my name is ${fullName}. I am learning ${language}. It is ${isCaseSensitive}, that it is case sensitive. JavaScript was made in ${createdYear}! `;
+console.log(myStory);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+let theStore = `Hi, we don't have the book ${title} by ${author}. It has ${pageCount} pages and currently, it's priced at ${price} at this other store.`;
